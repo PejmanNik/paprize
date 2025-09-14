700288fd408337daec6332962cd1c3814b36a588
Pejman Nikram <Pejmannikram@gmail.com>
2025-09-14 20:10:44 +0300

    fix: fix website base url

--- packages/website/docusaurus.config.ts
+++ packages/website/docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
     url: 'https://Paprize.page',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/paprize/',
+    baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
